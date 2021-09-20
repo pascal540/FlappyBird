e84189334df8e462e76dbde3389c1ca10f858e4b
Pascal Haas <marketing54320@gmail.com>
2021-09-20 15:28:19 +0200

    Flappy Bird Postionnement et deplacement des tuayux

--- src/com/Scene.java
+++ src/com/Scene.java
@@ -6,6 +6,7 @@ import javax.swing.JPanel;
 import com.objet.Tuyau;
 
 import java.awt.*;
+import java.util.Random;
 
 public class Scene extends JPanel {
 
@@ -31,6 +32,8 @@ public class Scene extends JPanel {
     public Tuyau tuyauHaut3;
     public Tuyau tuyauBas3;
 
+    private Random hasard;
+
     /**
      * CONSTRUCTEUR
      */
@@ -48,18 +51,15 @@ public class Scene extends JPanel {
         this._dxTuyaux = 0;
 
         this.tuyauHaut1 = new Tuyau(this._xTuyaux, -150, "/images/tuyauhaut.png");
-        this.tuyauBas1 = new Tuyau(this._xTuyaux, -250, "/images/tuyaubas.png");
+        this.tuyauBas1 = new Tuyau(this._xTuyaux, 250, "/images/tuyauBas.png");
 
-        this.tuyauHaut2 = new Tuyau(this._xTuyaux + this.tuyauBas1.get_largeur() + this.ECART_TUYAUX, -150,
-                "/images/tuyauhaut.png");
-        this.tuyauBas2 = new Tuyau(this._xTuyaux + this.tuyauHaut1.get_largeur() + this.ECART_TUYAUX, -150,
-                "/images/tuyaubas.png");
+        this.tuyauHaut2 = new Tuyau(this._xTuyaux + this.DISTANCE_TUYAUX, -100, "/images/tuyauhaut.png");
+        this.tuyauBas2 = new Tuyau(this._xTuyaux + this.DISTANCE_TUYAUX, 300, "/images/tuyaubas.png");
 
-        this.tuyauHaut3 = new Tuyau(this._xTuyaux + (this.tuyauBas1.get_largeur() + this.ECART_TUYAUX * 2), -150,
-                "/images/tuyauhaut.png");
-        this.tuyauBas3 = new Tuyau(this._xTuyaux + (this.tuyauHaut1.get_largeur() + this.ECART_TUYAUX * 2), -250,
-                "/images/tuyaubas.png");
+        this.tuyauHaut3 = new Tuyau(this._xTuyaux + this.DISTANCE_TUYAUX * 2, -120, "/images/tuyauhaut.png");
+        this.tuyauBas3 = new Tuyau(this._xTuyaux + this.DISTANCE_TUYAUX * 2, 280, "/images/tuyaubas.png");
 
+        hasard = new Random();
         Thread chronoEcran = new Thread(new Chrono());
         chronoEcran.start(); // depart chrono et affichage ecran
     }
@@ -79,8 +79,48 @@ public class Scene extends JPanel {
 
     }
 
+    private void deplacementTuyaux(Graphics g) {
+        // tuyau1
+        this.tuyauHaut1.set_x(this.tuyauHaut1.get_x() - 1);
+        this.tuyauBas1.set_x(this.tuyauHaut1.get_x());
+
+        if (this.tuyauHaut1.get_x() == -100) {
+            this.tuyauHaut1.set_x(this.tuyauHaut3.get_x() + this.DISTANCE_TUYAUX);
+            this.tuyauHaut1.set_y(-100 - (10 * this.hasard.nextInt(18)));
+            this.tuyauBas1.set_y(this.tuyauHaut1.get_y() + this.tuyauHaut1.get_hauteur() + this.ECART_TUYAUX);
+        }
+        g.drawImage(this.tuyauHaut1.get_imgTuyau(), this.tuyauHaut1.get_x(), this.tuyauHaut1.get_y(), null);
+        g.drawImage(this.tuyauBas1.get_imgTuyau(), this.tuyauBas1.get_x(), this.tuyauBas1.get_y(), null);
+
+        // tuyau2
+        this.tuyauHaut2.set_x(this.tuyauHaut2.get_x() - 1);
+        this.tuyauBas2.set_x(this.tuyauHaut2.get_x());
+
+        if (this.tuyauHaut2.get_x() == -100) {
+            this.tuyauHaut2.set_x(this.tuyauHaut1.get_x() + this.DISTANCE_TUYAUX);
+            this.tuyauHaut2.set_y(-100 - (10 * this.hasard.nextInt(18)));
+            this.tuyauBas2.set_y(this.tuyauHaut2.get_y() + this.tuyauHaut2.get_hauteur() + this.ECART_TUYAUX);
+        }
+        g.drawImage(this.tuyauHaut2.get_imgTuyau(), this.tuyauHaut2.get_x(), this.tuyauHaut2.get_y(), null);
+        g.drawImage(this.tuyauBas2.get_imgTuyau(), this.tuyauBas2.get_x(), this.tuyauBas2.get_y(), null);
+
+        // // tuyau3
+        this.tuyauHaut3.set_x(this.tuyauHaut3.get_x() - 1);
+        this.tuyauBas3.set_x(this.tuyauHaut3.get_x());
+
+        if (this.tuyauHaut3.get_x() == -100) {
+            this.tuyauHaut3.set_x(this.tuyauHaut2.get_x() + this.DISTANCE_TUYAUX);
+            this.tuyauHaut3.set_y(-100 - (10 * this.hasard.nextInt(18)));
+            this.tuyauBas3.set_y(this.tuyauHaut3.get_y() + this.tuyauHaut3.get_hauteur() + this.ECART_TUYAUX);
+        }
+        g.drawImage(this.tuyauHaut3.get_imgTuyau(), this.tuyauHaut3.get_x(), this.tuyauHaut3.get_y(), null);
+        g.drawImage(this.tuyauBas3.get_imgTuyau(), this.tuyauBas3.get_x(), this.tuyauBas3.get_y(), null);
+
+    }
+
     public void paintComponent(Graphics g) {
         this.deplacementFond(g);
+        this.deplacementTuyaux(g);
 
     }
 }
